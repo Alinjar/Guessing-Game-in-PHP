447652b9f83a4eb2637b6887bb8ec21d48046b0f
Alinjar <tonydas66@gmail.com>
2021-09-22 12:43:58 +0530

    Add files via upload

---
+++ guessing game/index.php
@@ -0,0 +1,27 @@
+
+<title> Alinjar Das PHP </title>
+<h1> Alinjar Das PHP </h1>
+
+<?php 
+print ('Hash for Alinjar Das is ');
+print hash( 'sha256', 'Alinjar Das');
+?>
+<pre>
+Ascii Art:
+.---.                   |   |                   
+          |   |.--.   _..._       '---'                   
+          |   ||__| .'     '.     .---.                   
+          |   |.--..   .-.   .    |   |          .-,.--.  
+    __    |   ||  ||  '   '  |    |   |    __    |  .-. | 
+ .:--.'.  |   ||  ||  |   |  |    |   | .:--.'.  | |  | | 
+/ |   \ | |   ||  ||  |   |  |    |   |/ |   \ | | |  | | 
+`" __ | | |   ||  ||  |   |  |    |   |`" __ | | | |  '-  
+ .'.''| | |   ||__||  |   |  |    |   | .'.''| | | |      
+/ /   | |_'---'    |  |   |  | __.'   '/ /   | |_| |      
+\ \._,\ '/         |  |   |  ||      ' \ \._,\ '/|_|      
+ `--'  `"          '--'   '--'|____.'   `--'  `"          
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br>
+<a href="fail.php">Click here to cause a traceback</a>
+
